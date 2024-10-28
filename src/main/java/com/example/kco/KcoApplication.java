ee4142b397f9f6409a2e0f2e072c1d92db891839
KCO <kevin.cornot@gmail.com>
2024-10-28 22:10:22 +0100

    first

---
+++ src/main/java/com/example/kco/KcoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kco/KcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
